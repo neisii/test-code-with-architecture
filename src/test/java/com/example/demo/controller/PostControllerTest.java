60ab629cd101b448f3f8e65d14ac66fc405e3e27
NEISII <neisii@outlook.com>
2023-11-03 22:10:05 +0900

    userController 메소드에 테스트 코드 추가

---
+++ src/test/java/com/example/demo/controller/PostControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @Test
+    void getPostById() {
+    }
+
+    @Test
+    void updatePost() {
+    }
+
+    @Test
+    void toResponse() {
+    }
+}
